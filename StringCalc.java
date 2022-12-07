62f6490dabef51b439ef3e32f7bdc1def12600e6
MohamedHatem-0110 <83113802+MohamedHatem-0110@users.noreply.github.com>
2022-12-07 08:40:03 +0200

    Delimiter Test Done

--- StringCalc.java
+++ StringCalc.java
@@ -4,19 +4,30 @@ public class StringCalc {
             return 0;
         else if (numbers.length() > 2) {
             int sum = 0;
+            char delimiter = ' ';
+            if (numbers.length() >= 4) {
+                if (numbers.charAt(0) == '/' && numbers.charAt(1) == '/') {
+                    delimiter = numbers.charAt(2);
+                    numbers = numbers.substring(4, numbers.length());
+                }
+            }
             for (int i = 0; i < numbers.length(); i++) {
-                if (numbers.charAt(i) == '\n') {
+                char currentChar = numbers.charAt(i);
+                if (currentChar == delimiter) {
+                    currentChar = ',';
+                }
+                if (currentChar == '\n') {
                     i++;
-                    if (numbers.charAt(i) == ',') {
+                    if (currentChar == ',') {
                         return -1;
                     }
                 }
-                if (numbers.charAt(i) == ',') {
-                    if (numbers.charAt(i + 1) == '\n') {
+                if (currentChar == ',') {
+                    if (currentChar == '\n') {
                         return -1;
                     }
                 } else
-                    sum += Integer.parseInt(numbers.charAt(i) + "");
+                    sum += Integer.parseInt(currentChar + "");
             }
             return sum;
         } else {
