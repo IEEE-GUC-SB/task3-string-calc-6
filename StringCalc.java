909742e9139e822f1da1c86251e29a1573b15359
MohamedHatem-0110 <83113802+MohamedHatem-0110@users.noreply.github.com>
2022-12-07 07:13:48 +0200

    NewLine Test Done

--- StringCalc.java
+++ StringCalc.java
@@ -5,6 +5,9 @@ public class StringCalc {
         else if (numbers.length() >= 3) {
             int sum = 0;
             for (int i = 0; i < numbers.length(); i++) {
+                if (numbers.charAt(i) == '\n') {
+                    i++;
+                }
                 if (numbers.charAt(i) != ',') {
                     sum += Integer.parseInt("" + numbers.charAt(i));
                 }
