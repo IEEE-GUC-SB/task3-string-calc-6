8cd32066b93bc3c33dde12982492661292cd996d
MohamedHatem-0110 <83113802+MohamedHatem-0110@users.noreply.github.com>
2022-12-07 07:01:15 +0200

    TwoNumbersDone

--- StringCalc.java
+++ StringCalc.java
@@ -2,7 +2,9 @@ public class StringCalc {
     public int Add(String numbers) {
         if (numbers.equals(""))
             return 0;
-        else {
+        else if (numbers.length() == 3) {
+            return Integer.parseInt("" + numbers.charAt(0)) + Integer.parseInt("" + numbers.charAt(2));
+        } else {
             return Integer.parseInt(numbers);
         }
 
