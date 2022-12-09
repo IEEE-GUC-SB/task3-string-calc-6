d64ef94a1a6affc14174f8b0247bff2a1a17db51
MohamedHatem-0110 <83113802+MohamedHatem-0110@users.noreply.github.com>
2022-12-09 06:58:19 +0200

    Many Delimiters Solved

--- StringCalc.java
+++ StringCalc.java
@@ -7,16 +7,34 @@ public class StringCalc {
             char delimiter = ' ';
             String value = "";
             boolean flag = false;
+            String delimiterString = "";
             if (numbers.length() >= 4) {
                 if (numbers.charAt(0) == '/' && numbers.charAt(1) == '/') {
-                    delimiter = numbers.charAt(2);
-                    numbers = numbers.substring(4, numbers.length());
+                    if (numbers.charAt(2) == '[') {
+                        delimiter = numbers.charAt(3);
+                        int k = 3;
+                        while (numbers.charAt(k) != ']') {
+                            delimiterString += numbers.charAt(k);
+                            k++;
+                        }
+                        numbers = numbers.substring(k + 2, numbers.length());
+                    } else {
+                        delimiter = numbers.charAt(2);
+                        numbers = numbers.substring(4, numbers.length());
+                    }
                 }
             }
             for (int i = 0; i < numbers.length(); i++) {
                 char currentChar = numbers.charAt(i);
                 if (currentChar == delimiter) {
+                    if (!delimiterString.equals("")) {
+                        while (numbers.charAt(i + 1) == delimiter) {
+                            i++;
+                        }
+                        flag = true;
+                    }
                     currentChar = ',';
+
                 }
                 if (currentChar == '\n') {
                     currentChar = ',';
@@ -37,7 +55,8 @@ public class StringCalc {
                     // flag = false;
                     // }
 
-                } else {
+                }
+                if (currentChar != ',') {
                     value += currentChar;
                     if (flag == false || i == numbers.length() - 1 || numbers.charAt(i + 1) == ',') {
                         if (Integer.parseInt(value + "") <= 1000) {
